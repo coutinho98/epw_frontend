1b8fee696fc8679e2f448cd37ffc1828e518070d
Mateus Couto <mateuscouto98@outlook.com>
2025-07-20 16:16:02 -0300

    vite api url changed

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://epw-mvkp.onrender.com';
 
 interface RequestOptions extends RequestInit {
     headers?: HeadersInit;
